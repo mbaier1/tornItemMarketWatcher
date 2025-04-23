de17f3fa17d5a158c6e7192cc8d6a350275340df
mbaier <mbaier@K1.>
2025-04-22 23:44:10 -0400

    rules

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,9 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/promise-function-async": "error",
     },
   },
 )
