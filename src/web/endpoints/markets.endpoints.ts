0264f6844fe368157834fc4af8fd192a16eb1a41
mbaier <mbaier@K1.>
2025-04-28 22:17:19 -0400

    forgot to save the endpoints ts file

--- src/web/endpoints/markets.endpoints.ts
+++ src/web/endpoints/markets.endpoints.ts
@@ -1,8 +1,8 @@
-import { tornBaseUri } from "../constants/uris.constants"
+// import { tornBaseUri } from "../constants/uris.constants"
 
-TODO: Write the endpoint
-export const getItemMarketListingsByItemId = async (id: number) => {
-    const url = "${tornBaseUri}/market/${id}/itemmarket?offset=0"
-    const request = new Request()
-    const result = await fetch(url, );
-}
+// TODO: Write the endpoint
+// export const getItemMarketListingsByItemId = async (id: number) => {
+//     const url = `${tornBaseUri}/market/${id}/itemmarket?offset=0`;
+//     const request = new Request()
+//     const result = await fetch(url, );
+// }
